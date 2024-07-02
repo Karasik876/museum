9f2a270528fd846503d11bdaf6f478a3684dd2b7
Karasik876 <nik.pou@mail.ru>
2024-07-02 22:27:36 +0500

    Initial constructor commit

--- backend/core/entrypoint.sh
+++ backend/core/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
@@ -8,5 +8,4 @@ do
   echo "Waiting for db to be ready..."
   sleep 2
 done
-
 exec "$@"
